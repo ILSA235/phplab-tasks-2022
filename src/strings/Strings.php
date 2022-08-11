3891cbee3064fe087ccad0033c6ef0a1265879e0
Andrii Korotkykh <arta5472@gmail.com>
2022-08-11 15:36:29 +0300

    Second commit

--- src/strings/Strings.php
+++ src/strings/Strings.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace strings;
 
-class Strings implements StringsInterface{
-	
-	public function snakeCaseToCamelCase(string $input): string{
+class Strings implements StringsInterface
+{
+	public function snakeCaseToCamelCase(string $input): string
+	{
 		$vrb = $input;	
 		$vrb = strtr($vrb,"_"," ");
 		$vrb = ucwords($vrb);
@@ -12,7 +14,8 @@ class Strings implements StringsInterface{
 		return $vrb;
 	}
 	
-	public function mirrorMultibyteString(string $input): string{
+	public function mirrorMultibyteString(string $input): string
+	{
 		$input = iconv('utf-8', 'utf-16le', $input);
 		$input = strrev($input);
 		$input = iconv('utf-16be', 'utf-8', $input);
@@ -22,13 +25,12 @@ class Strings implements StringsInterface{
 		return $input;
 	}
 	
-	public function getBrandName(string $noun): string{
-		if($noun[0] === $noun[-1]){
+	public function getBrandName(string $noun): string
+	{
+		if ($noun[0] === $noun[-1]) {
 			return ucfirst($noun) . substr($noun, 1);
-		}
-		else if($noun[0] != $noun[-1]){
+		} else if ($noun[0] != $noun[-1]) {
 			return "The" . " " . ucfirst($noun);
 		}
 	}
 }
-?>
