6a8d5affac75f8681193aa69692fd7e88f932681
ILSA235 <99665852+ILSA235@users.noreply.github.com>
2022-09-08 23:12:41 +0300

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-		require_once "vendor/autoload.php";
-		
-		use GuzzleHttpClient;
-		
-        $client = new GuzzleHttp\Client();
-        $res = $client->request('GET', 'https://kinoukr.com/4166-pravdyva-istoriya-bandy-kelli.html');
-        var_dump($res);
